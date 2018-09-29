72b9c31c765fa192677bbd56806c0412ce776165
Pascal Macchi <pascal.macchi@gmail.com>
2018-09-29 22:15:10 +0200

    edit main.py file and product.py file 'insert data in BD'

--- product/category/product.py
+++ product/category/product.py
@@ -1,56 +1,112 @@
 # -*- coding: utf-8 -*-
 
 import json
-import mysql.connector
+
 
 """module containing the product characteristics of JSON (API OPENFOODFACT)"""
 
-def insert_product(category, nb_product):
+def contained_database(data, category):
 	
 	#
-	# opening database
+	# read data from the json file
 	#
-
-	database= mysql.connector.connect(host="localhost",
-									  user="student",
-									  password="OpenClassRooms",
-									  database="food_product"
-	)
-	cursor = database.cursor()
-	
-	#
-	# filling database
-	#
-	
-	with open(category) as json_category:
+	with open(data) as json_category:
 		category_dict = json.load(json_category)
 	
 	name = ''
 	grade = ''
-	try:
-		if "generic_name_fr" in category_dict['products'][nb_product].keys():
-			name = category_dict['products'][nb_product]['generic_name_fr']
-		elif "generic_name" in category_dict['products'][nb_product].keys():
-			name = category_dict['products'][nb_product]['generic_name']
+	nb_product = 1
+	while nb_product <= category_dict['page_size']:
+		try:
+			if "generic_name_fr" in category_dict['products'][nb_product].keys():
+				name = category_dict['products'][nb_product]['generic_name_fr']
+			elif "generic_name" in category_dict['products'][nb_product].keys():
+				name = category_dict['products'][nb_product]['generic_name']
 		
-		if "nutrition_grades" in category_dict['products'][nb_product].keys():
-			grade = category_dict['products'][nb_product]['nutrition_grades']
-		elif "nutrition_grades_fr" in category_dict['products'][nb_product].keys():
-			grade = category_dict['products'][nb_product]['nutrition_grades_fr']
+			if "nutrition_grades" in category_dict['products'][nb_product].keys():
+				grade = category_dict['products'][nb_product]['nutrition_grades']
+			elif "nutrition_grades_fr" in category_dict['products'][nb_product].keys():
+				grade = category_dict['products'][nb_product]['nutrition_grades_fr']
 			
-		sql = "INSERT INTO product (url, nutrition_grade, name, store) VALUES (%s, %s, %s, %s)"
-		val = ( category_dict['products'][nb_product]['url'],
-				grade, name, category_dict['products'][nb_product]['stores']
-		)
+			#
+			# product data
+			#
+		    
+		    val_product = (
+				category_dict['products'][nb_product]['product_name'],
+				category_dict['products'][nb_product]['url'],
+				category_dict['product'][nb_product]['stores'],
+				name, 
+				grade
+		    )
+						   				         		              			
+			contained_product(val_product) # insert product data
+			
+			#
+			# category data
+			#
+			val_category = (category,)
+			
+			contained_category(val_category) # insert category data
+			
+			
+		except IndexError: 
+			pass
+		except KeyError:
+			pass
+								
+		nb_product += 1
+
+
+"""module containing the insertion of product data"""
+
+def insert_product(data):
 	
-		cursor.execute(sql, val)
-		database.commit()
+	#
+	# database filling 
+	#
+	cursor.execute("""INSERT INTO product(url, nutrition_grade, description, name, store) VALUES (%s, %s, %s, %s, %s)""", data)
 			
-	except IndexError: # List ==> OUT OF RANGE 
+	database.commit()	
+		
+
+"""module containing the insertion of product category"""
+
+
+def insert_category(data):
+	
+	#
+	# database filling 
+	#
+	cursor.execute("""INSERT INTO category(category) VALUES(%s)""", data)
+			
+	database.commit()	
+		
+	
+"""test module and product insertion"""
+
+def contained_product(data):
+	
+	cursor.execute("""SELECT * FROM product """)  
+	if not cursor.fetchall():
+		insert_product(data)
+	else:
 		pass
+		
+	insert_product(data)
+
+
+"""test module and category insertion"""
+
+def contained_category(data):
 	
-	#
-	# closing database
-	#
-	
-	database.close()
+	cursor.execute("""SELECT * FROM category """)
+	rows = cursor.fetchall()
+	if not rows:
+		insert_category(data)
+	else:
+		for row in rows:
+			if row != category:
+				insert_category(data)
+			else:
+				pass	
--- product/main.py
+++ product/main.py
@@ -1,13 +1,27 @@
 # -*- coding:utf-8 -*-
 
+import mysql.connector
 from category.product import *
 
 print('')
 print ('le programme de Substitution de produit est lancé ! ')
 print('')
-#
-#  Product selection menu
-#
+
+#--------------------
+#| Opening DataBase |
+#--------------------
+
+database= mysql.connector.connect(host="localhost",
+								  user="student",
+								  password="OpenClassRooms",
+								  database="food_product"
+)
+cursor = database.cursor()
+
+#----------------------------
+#|  Product selection menu  |
+#----------------------------
+
 choix =[['Boissons_energetiques','boissons_energie.json'],['Bonbons','bonbons.json'],
 		['Charcuteries','charcuteries.json'],['Chocolats','chocolats.json'],
 	    ['Conserves','conserves.json'],['Fromages','fromages.json'],
@@ -20,15 +34,19 @@ choix =[['Boissons_energetiques','boissons_energie.json'],['Bonbons','bonbons.js
 	    ['Riz','riz.json'],['Sorbets','sorbets.json'],['Viandes','viandes.json'],
 	    ['Vins','vins.json'],['Yaourts','yaourts.json']
 ]
-
+name = ''
 for i, elt in enumerate(choix):
+	
 	print("{}. {}".format(i+1, elt[0]))
-	insert_product('category/' + choix[i][1],i)
-
+	contained_database('category/' + choix[i][1], choix[i][0])
+		
 while True:
+    
     category = input('choississez une catégory de produit par son numéro : ')
+    
     try:
         category = int(category)
+        
     except ValueError:
         print("Vous devez choisir un nombre")
     else:
@@ -37,7 +55,14 @@ while True:
         else:
             break 
 
+name = choix[category-1][0]
+print(name)
 
+# read_product(name)
 
+#--------------------
+#| closing database |
+#--------------------
 
+database.close()
 
