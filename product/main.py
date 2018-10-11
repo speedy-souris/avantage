b963b530223f7a550ed85426061529e10b5cdb8e
Pascal Macchi <pascal.macchi@gmail.com>
2018-10-11 19:12:47 +0200

    restructuring of the script: module creation function creation of the process module json directory creation insert category before insert product line 29 to 35 in insert.py file

--- product/main.py
+++ product/main.py
@@ -1,53 +1,9 @@
 # -*- coding:utf-8 -*-
 
-from category.product import *
+from process import menu_category as m_cat
 
 print('')
 print('le programme de Substitution de produit est lancé ! ')
 print('')
 
-erase_data()  # cleaning database
-
-# ----------------------------
-# |  Product selection menu  |
-# ----------------------------
-
-choix = [
-    ['Boissons_energetiques', 'boissons_energie.json'], [
-        'Bonbons', 'bonbons.json'],
-    ['Charcuteries', 'charcuteries.json'], ['Chocolats', 'chocolats.json'],
-    ['Conserves', 'conserves.json'], ['Fromages', 'fromages.json'],
-    ['Fruits', 'fruits.json'], ['Fruits_confits', 'fruits_confits.json'],
-    ['Gateaux', 'gateaux.json'], ['Glaces', 'glaces.json'],
-    ['Jus_de_fruits', 'jus_fruits.json'], ['Laits', 'laits.json'],
-    ['Pates_de_fruit', 'pate_fruits.json'], ['Pates', 'pates.json'],
-    ['Pates_a_tartiner', 'pates_tartiner.json'], ['Pizzas', 'pizzas.json'],
-    ['Poissons', 'poissons.json'], ['Poissons_elevages',
-                                    'poissons_elevage.json'],
-    ['Reglisse', 'reglisses.json'], ['Riz', 'riz.json'],
-    ['Sorbets', 'sorbets.json'], ['Viandes', 'viandes.json'],
-    ['Vins', 'vins.json'], ['Yaourts', 'yaourts.json']
-]
-name = ''
-for i, elt in enumerate(choix):
-
-    contained_database('category/' + choix[i][1], choix[i][0], i)
-    print("{}. {}".format(i+1, elt[0]))
-
-while True:
-
-    category = input('choississez une catégory de produit par son numéro : ')
-
-    try:
-        category = int(category)
-
-    except ValueError:
-        print("Vous devez choisir un nombre")
-    else:
-        if not 0 < category < 25:
-            print("La catégorie doit être entre 1 et 24")
-        else:
-            break
-
-name = choix[category-1][0]
-print(name)
+m_cat.menu_category()
